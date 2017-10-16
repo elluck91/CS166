8ec9c2221f4f28d5a55f4b5cafd20316d27429e1
elluck91 <juraszeklukasz@gmail.com>
2017-10-16 21:20:38 +0000

    Added reading secret from a bitmap file.

--- BMP.java
+++ BMP.java
@@ -1,14 +1,32 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BMP {
     public static void main(String[] args) throws IOException {
-        File bmpFile = new File("alice_stego.bmp");
-        BufferedImage image = ImageIO.read(bmpFile);
-        System.out.print(image);
+//    	File bmpFile = new File("alice_stego.bmp");
+//    	BufferedImage image = ImageIO.read(bmpFile);
+    	byte[] pt = Files.readAllBytes( Paths.get("alice_stego.bmp") );
+    	String binaryString = "";
+    	String temp;
+    	int[] binaryCharacter = new int[8];
+    	int size = 0;
+
+    	for (int i = 0; i < 1200; i++) {
+    		if (size < 8) {
+    			temp = String.format("%8s", Integer.toBinaryString(pt[i] & 0xFF)).replace(' ', '0');
+    			binaryCharacter[size] = temp.charAt(7);
+    			size++;
+    		}
+    		if (size == 8) {
+    			size = 0;
+    			for (int a = 7; a >= 0; a--) {
+					binaryString += Character.getNumericValue(binaryCharacter[a]);
+				}
+    			System.out.print("Output: "+ Integer.parseInt(binaryString,2) + " | Character: " + (char)Integer.parseInt(binaryString,2) + "\n");
+    			binaryString = "";
+    		}
+    	}
+
     }
-
-
 }
