169eeeeadc3f9d3a4fd12b7b94fd4bc25fd011db
elluck91 <juraszeklukasz@gmail.com>
2017-10-16 21:56:45 +0000

    Implemented dictionary attach

--- Java/MD5.java
+++ Java/MD5.java
@@ -1,40 +1,86 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.security.MessageDigest;
-
 public class MD5 {
 
 	public static void main(String[] args) throws Exception {
-		byte[] data = "abcdefgh".getBytes();
-		String temp;
-		String hash = "419ee836f207c09d92566feaa14320d2";
-		MessageDigest digest = MessageDigest.getInstance("MD5");
-		Utils.printHex( digest.digest(data) );
+		String current = new java.io.File( "." ).getCanonicalPath();
+        System.out.println("Current dir:"+current);
+		//File file = new File("passworddictionary.txt");
+		FileReader fr = new FileReader(current + "/passworddictinary.txt");
+		BufferedReader br = new BufferedReader(fr);
 
-		for (int i = 1; i < 64001; i++) {
-			temp = Integer.toString(i);
-			data = temp.getBytes();
+		String line;
+		String temp;
+		String hash = "79bb8d29bad9c9534b5b0d154febf0cec5efbdb9d15821bb6675af2636a061d2";
+		boolean check = true;
+		MessageDigest digest = MessageDigest.getInstance("MD5");
+		byte[] data;
+
+		while ((line = br.readLine()) != null && check) {
+			data = line.getBytes();
 			temp = ba2string( digest.digest(data) );
 			if (temp.equals(hash)) {
-				System.out.println(i);
+				System.out.println(line);
+				check = false;
+				break;
+			}
+		}
+		br.close();
+		br = new BufferedReader(new FileReader(current + "/passworddictinary.txt"));
+		digest = MessageDigest.getInstance("SHA-1");
+
+		while ((line = br.readLine()) != null && check) {
+			data = line.getBytes();
+			temp = ba2string( digest.digest(data) );
+			if (temp.equals(hash)) {
+				System.out.println(line);
+				check = false;
 				break;
 			}
 		}
 
-
-
-		/*
-
-		digest = MessageDigest.getInstance("SHA-1");
-		Utils.printHex( digest.digest(data) );
-
+		br.close();
+		br = new BufferedReader(new FileReader(current + "/passworddictinary.txt"));
 		digest = MessageDigest.getInstance("SHA-256");
-		Utils.printHex( digest.digest(data) );
 
+		while ((line = br.readLine()) != null && check) {
+			data = line.getBytes();
+			temp = ba2string( digest.digest(data) );
+			if (temp.equals(hash)) {
+				System.out.println(line);
+				check = false;
+				break;
+			}
+		}
+
+		br.close();
+		br = new BufferedReader(new FileReader(current + "/passworddictinary.txt"));
 		digest = MessageDigest.getInstance("SHA-384");
-		Utils.printHex( digest.digest(data) );
 
+		while ((line = br.readLine()) != null && check) {
+			data = line.getBytes();
+			temp = ba2string( digest.digest(data) );
+			if (temp.equals(hash)) {
+				System.out.println(line);
+				check = false;
+				break;
+			}
+		}
+
+		br.close();
+		br = new BufferedReader(new FileReader(current + "/passworddictinary.txt"));
 		digest = MessageDigest.getInstance("SHA-512");
-		Utils.printHex( digest.digest(data) );
-		*/
+
+		while ((line = br.readLine()) != null && check) {
+			data = line.getBytes();
+			temp = ba2string( digest.digest(data) );
+			if (temp.equals(hash)) {
+				System.out.println(line);
+				check = false;
+				break;
+			}
+		}
 	}
 
 	public static String ba2string(byte[] ba) {
